9fcb0a8e4c3f6e7ca710dde86d2a3f93d118664c
chiedozieu <chiedozieugwor@gmail.com>
2025-05-19 11:33:29 -0400

    complete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -6,9 +6,34 @@ function App() {
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
+  const [copied, setCopied] = useState(false);
+
+  const passwordRef = useRef(null);
+
+  const generatePassword = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%&*_-+=/|()`~";
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed]);
+
+  useEffect(() => {
+    generatePassword();
+  }, [length, numberAllowed, charAllowed]);
+
+  const copyPasswordToClipboard = () => {
+    window.navigator.clipboard.writeText(password);
+    passwordRef.current?.select();
+    setCopied(true);
+  };
 
   return (
-    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-8 mt-8">
+    <div className="w-full max-w-lg mx-auto shadow-md rounded-lg px-4 py-8 mt-8 bg-gray-800">
       <h1 className="text-2xl font-bold text-center mb-8 text-white">
         Password Generator
       </h1>
@@ -21,26 +46,33 @@ function App() {
           readOnly
           value={password}
           className="outline-none w-full py-1 px-3"
+          ref={passwordRef}
         />
-        <button className="outline-none bg-amber-400 px-3 py-1 hover:bg-amber-500 cursor-pointer">
-          Copy
+
+        <button
+          onClick={copyPasswordToClipboard}
+          className={` outline-none ${
+            copied ? "bg-green-400" : "bg-amber-400 hover:bg-amber-500"
+          } px-3 py-1  cursor-pointer`}
+        >
+          {copied ? "Copied" : "Copy"}
         </button>
       </div>
       <div className="flex text-sm gap-x-2 items-center">
-        <div className="text-[#ffffffc4] p-2 flex items-center gap-x-2">
+        <div className="text-[#ffffffc4] flex items-center gap-x-2">
           <input
             type="range"
-            className="h-2 bg-amber-200 rounded-full appearance-none cursor-pointer "
+            className="h-2 rounded-full cursor-pointer accent-amber-400 hover:accent-amber-500"
             value={length}
             onChange={(e) => setLength(e.target.value)}
             name=""
             id="length"
-            max={30}
+            max={50}
             min={6}
           />
           <label htmlFor="length">Length: {length}</label>
         </div>
-        <div className="text-[#ffffffc4] p-2 flex items-center gap-x-1">
+        <div className="text-[#ffffffc4] flex items-center gap-x-1 ml-auto">
           <input
             type="checkbox"
             defaultChecked={numberAllowed}
@@ -52,7 +84,7 @@ function App() {
           />
           <label htmlFor="number">Numbers</label>
         </div>
-        <div className="text-[#ffffffc4] p-2 flex items-center gap-x-1">
+        <div className="text-[#ffffffc4] p-2 flex items-center gap-x-1 ml-auto">
           <input
             type="checkbox"
             defaultChecked={charAllowed}
@@ -65,7 +97,6 @@ function App() {
           <label htmlFor="charInput">Characters</label>
         </div>
       </div>
-      
     </div>
   );
 }
